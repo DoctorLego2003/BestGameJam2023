f5dfa96535cac6e3102754401a90cd3b8de58825
Benneth Paredis <benneth.paredis@student.kuleuven.be>
2023-10-28 20:11:27 +0200

    added wave system

--- BestGameJam/scripts/enemySpawner.gd
+++ BestGameJam/scripts/enemySpawner.gd
@@ -5,18 +5,33 @@ extends Node
 @export var intervalSpawn : float
 @export var amountOfEnemies : int
 
+var timer : float
 var path
-
+var amountSpawned = 0
+var inWave = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	path = get_node("path")
-	spawn()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if inWave:
+		if (amountSpawned < amountOfEnemies):
+			timer += delta
+			if(timer >= intervalSpawn):
+				spawn()	
+				amountSpawned += 1
+				timer = 0
+		else:
+			amountSpawned = 0
+			inWave = false
+	else:
+		timer += delta
+		if(timer >= cooldownWave):
+			timer = 0
+			inWave = true
 
 func spawn():
 	var spawnedEnemy = minion.instantiate()
