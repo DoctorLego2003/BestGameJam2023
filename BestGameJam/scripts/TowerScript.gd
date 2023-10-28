48aa1eead59190e47e57ad3bf424b597c4678e1f
Benneth Paredis <benneth.paredis@student.kuleuven.be>
2023-10-29 01:15:56 +0200

    worked on tower

--- BestGameJam/scripts/TowerScript.gd
+++ BestGameJam/scripts/TowerScript.gd
@@ -1,13 +1,34 @@
 extends Node
 
+@export var arrow : PackedScene
+var shootingRange = 2
+
+var currentTargets = []
+var currentTarget
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+# ==== SHOOTING PART ======= #
+func _on_aggro_area_entered(area):
+	if area.is_in_group("enemy"):
+		currentTarget.append(area.get_parent())
+		print("enemy entered range!")
+	
+func _on_aggro_area_exited(area):
+	if area.is_in_group("enemy"):
+		currentTarget.erase(area.get_parent())
+		print("enemy exited range!")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass 
+func shootTarget():
+	var arrowInstance = arrow.instantiate()
+	add_child(arrowInstance)
+
+# ========================== #
+
+# ==== PLACING PART ======= #
+
 
 func _get_drag_data(at_position):
 	return at_position
@@ -17,4 +38,8 @@ func _can_drop_data(_pos, data):
 
 func _drop_data(_pos, data):
 	pass
+	
+# ========================== #
 
+func _process(delta):
+	pass 
