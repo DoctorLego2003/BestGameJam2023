13c6f4e00cbd1206addd70f7f9a5c65641607065
Pepijn Haenen <pepijn.haenen@gmail.com>
2023-10-29 10:41:16 +0100

    .

--- BestGameJam/scripts/main.gd
+++ BestGameJam/scripts/main.gd
@@ -11,5 +11,6 @@ func _process(delta):
 	if self.get_meta("Lives") <= 0:
 		print('dead')
 	if self.get_meta("TowerIsClicked"):
-		print('Tower Clicked')
-		print(self.get_meta("ClickedTowerPath"))
+		#show UI layer
+		
+	if !self.get_meta("TowerIsClicked"):
