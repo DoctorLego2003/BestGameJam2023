8b38f9ea97c5a2050dfe3d1a393ae9c8b6f033cd
Pepijn Haenen <pepijn.haenen@gmail.com>
2023-10-29 10:23:19 +0100

    Swapping werkt

--- BestGameJam/scripts/TowerText.gd
+++ BestGameJam/scripts/TowerText.gd
@@ -7,6 +7,7 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 			var main = get_node("/root/main")
 			main.set_meta("TowerIsClicked", true)
 			main.set_meta("ClickedTowerPath", get_parent().get_path())
+			main.set_meta("ClickedTowerPos", get_parent().position)
 
 func _on_area_3d_mouse_exited():
 	visible = false
