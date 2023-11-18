1929596afa92142329a50679adda1434f41f4d23
Pepijn Haenen <pepijn.haenen@gmail.com>
2023-11-18 01:14:34 +0100

    UI Heart update

--- BestGameJam/scripts/tiles/CastleText.gd
+++ BestGameJam/scripts/tiles/CastleText.gd
@@ -3,9 +3,9 @@ extends TextEdit
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_MASK_LEFT and event.pressed:
-			visible = true
 			var main = get_node("/root/main")
 			main.set_meta("TowerIsClicked", false)
+			visible = true
 
 func _on_area_3d_mouse_exited():
 	visible = false
